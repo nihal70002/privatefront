6d4b4f96b0edad156415915c46accc7e616ad305
nihal <niahlmukkum@gmail.com>
2026-01-14 11:37:32 +0530

    connect front to railway

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7188/api",
+  baseURL: "https://privateecommerce-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json"
   }
