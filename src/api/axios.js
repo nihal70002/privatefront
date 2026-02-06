343df585981d64c1d7eca993a02d64144313ff5b
nihal70002 <nihalahd7000@gmail.com>
2026-02-06 11:19:41 +0530

    Add axios auth interceptor

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,10 +5,22 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: false,
 });
 
-// 🔎 Debug
+// ✅ THIS IS THE MISSING PIECE
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 console.log("API URL 👉", import.meta.env.VITE_API_URL);
 
 export default api;
